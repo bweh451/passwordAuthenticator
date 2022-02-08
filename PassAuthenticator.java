d9ab6b084bed25a95c4d3750ed12b2cf05c708f9
bweh451 <thebrandi451@gmail.com>
2022-02-08 08:49:51 +0200

    Broke up the try catch block into two for better efficiency and readability

--- PassAuthenticator.java
+++ PassAuthenticator.java
@@ -24,25 +24,35 @@ public class PassAuthenticator {
 			
 			//Writes to the "password.txt" file.
 			f = new Formatter(password);
+		}
+		
+		//Catch block.
+		//If the "password.txt" does not exist of can't be found then if will print out the following.
+		catch(FileNotFoundException e) {
+			System.out.println("Error - file not found!");
+		}
 			
-			//String that prompts the user to enter the password they wish to create (first user input).
-			String pass = JOptionPane.showInputDialog("Please enter the password you would like to create: \n\n(Note: Your password must contain atleast one number"
-					+ " and your password cannot include the special character '%')");
-			
-			//String that prompts the user to re-enter the password they have entered in the first prompt in order to confirm it's creation (second user input).
-			String validatePass = JOptionPane.showInputDialog("Please re-enter the password in order to confirm the creation:");		
-			
-			//Created to boolean variables related to both strings above.
-			boolean passHasNum = false;
-			boolean validatePassHasNum = false;
-			
-			passHasNum = checkNum(pass, passHasNum);
-			
-			validatePassHasNum = checkNum(validatePass, validatePassHasNum);
-			
-			//Created a variable that stores the following string.
-			String entries = "Your first entry: " + pass + "\nYour second entry: " + validatePass;
-			
+		//String that prompts the user to enter the password they wish to create (first user input).
+		String pass = JOptionPane.showInputDialog("Please enter the password you would like to create: \n\n(Note: Your password must contain atleast one number"
+				+ " and your password cannot include the special character '%')");
+		
+		//String that prompts the user to re-enter the password they have entered in the first prompt in order to confirm it's creation (second user input).
+		String validatePass = JOptionPane.showInputDialog("Please re-enter the password in order to confirm the creation:");		
+		
+		//Created to boolean variables related to both strings above.
+		boolean passHasNum = false;
+		boolean validatePassHasNum = false;
+		
+		passHasNum = checkNum(pass, passHasNum);
+		
+		validatePassHasNum = checkNum(validatePass, validatePassHasNum);
+		
+		//Created a variable that stores the following string.
+		String entries = "Your first entry: " + pass + "\nYour second entry: " + validatePass;
+		
+		//Code is entered into a try-catch block to manage exceptions.
+		try {
+		
 			//Main if statement.
 			//If the both of the user's inputs contains a number then it will write both of the user's inputs to the "password.txt" file.
 			if(passHasNum && validatePassHasNum) {
@@ -80,19 +90,13 @@ public class PassAuthenticator {
 				
 		}
 		
-		//Catch block.
-		//If the "password.txt" does not exist of can't be found then if will print out the following.
-		catch(FileNotFoundException e) {
-			System.out.println("Error - file not found!");
-		}
-		
-		//Second catch block.
+		//First catch block.
 		//If the user uses the special character "%" then it will print out the following.
 		catch(UnknownFormatConversionException e) {
 			System.out.println("Your password creation was unsuccessful. \nPlease refrain from using '%' in you password! \nPlease try again.");
 		}
 		
-		//Third catch block.
+		//Second catch block.
 		//If the user presses "cancel" on one of their entries it will print out the following message.
 		catch(NullPointerException e) {
 			System.out.println("Your password creation was unsuccessful. \nYou have cancelled your entry. \nPlease try again.");
